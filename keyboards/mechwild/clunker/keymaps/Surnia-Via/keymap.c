57c90efdc3433fb2ee8fba3d93a9df1dc8d93c11
nobiliana <88566353+nobiliana@users.noreply.github.com>
2022-10-19 09:17:36 -0400

    Update keymap.c

--- keyboards/mechwild/clunker/keymaps/Surnia-Via/keymap.c
+++ keyboards/mechwild/clunker/keymaps/Surnia-Via/keymap.c
@@ -3,14 +3,6 @@
 
 #include QMK_KEYBOARD_H
 
-// Defines names for use in layer keycodes and the keymap
-
-enum layer_names {
-  _BASE,
-  _FN1,
-  _FN2,
-  _FN3
-};
 
 typedef struct {
   bool is_press_action;
@@ -28,6 +20,16 @@ enum {
 enum {
   X_LAYERS = 0
 };
+
+// Defines names for use in layer keycodes and the keymap
+
+enum layer_names {
+  _BASE,
+  _FN1,
+  _FN2,
+  _FN3
+};
+
 
 int cur_dance (qk_tap_dance_state_t *state);
 
@@ -47,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     k50, k51, k52,      k54,      k56,           k59, k5A, k5B  \
     */
     [_BASE] = LAYOUT(
-    KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_MUTE,
+    KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_MUTE,
     KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_BSPC,
     KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,              KC_ENT,
     KC_LSFT,  KC_SLSH, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_RSFT,
@@ -55,24 +57,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_FN1] = LAYOUT(
     KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,   HPT_FBK, HPT_TOG,
-    _______, KC_1,    KC_2,    KC_3,    KC_4,     KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-    _______, _______, _______, _______, _______,  _______, _______, KC_LBRC, KC_RBRC, KC_SCLN,          KC_QUOT,
-    _______, _______, _______, _______, _______,  _______, _______, _______, KC_MINS, KC_EQL,  KC_UP,   KC_SLSH,
-    _______, _______, _______,          _______,           _______,                   KC_LEFT, KC_DOWN, KC_RIGHT
+    _______, KC_ESC,  KC_PGUP, KC_GRV,  KC_4,     KC_P7,   KC_P8,   KC_P9,   KC_P0,   C(KC_C), C(KC_V),  KC_DEL,
+    _______, KC_HOME, KC_PGDN, KC_END,  C(KC_F),  KC_P4,   KC_P5,   KC_P6,   KC_PPLS, KC_PAST,           KC_BSLS,
+    _______, KC_BSPC, KC_SLSH, KC_MINS, KC_EQL,   KC_TAB,  KC_P1,   KC_P2,   KC_P3,   KC_PMNS, KC_PSLS,  _______,
+    _______, _______, _______,          _______,           KC_PDOT,                   _______, _______,  _______
     ),
     [_FN2] = LAYOUT(
-    QK_BOOT, _______, _______, _______, _______,  _______, _______, _______,
-    _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______,          _______,
-    _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______,          _______,           _______,                   _______, _______, _______
+    _______, _______, _______, _______, _______,  _______, _______, _______,
+    _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,   KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC,  KC_PSCR,
+    _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,    KC_DLR,  KC_PERC, KC_CIRC, KC_SCLN, KC_QUOT,           KC_CAPS,
+    _______, KC_DEL,  KC_F1,   KC_F2,   KC_F3,    KC_F4,   KC_EXLM, KC_AT,   KC_HASH, _______, _______,  KC_SCRL,
+    _______, _______, _______,          _______,           KC_ENT,                    KC_INS,  _______,  KC_NUM
     ),
 	[_FN3] = LAYOUT(
     _______, _______, _______, _______, _______,  _______, _______, _______,
-    _______, _______, _______, _______, _______,  _______, _______, _______, KC_UP, _______, _______, _______,
-    _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______,          _______,
-    _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______,          _______,           _______,                   _______, _______, _______
+    _______, _______, _______, _______, _______,  _______, KC_PGUP, KC_HOME, KC_UP,   KC_END,  _______,  _______,
+    _______, _______, _______, _______, _______,  _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT,           _______,
+    _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______,  _______,
+    _______, _______, _______,          _______,           _______,                   _______, _______,  _______
     )
 };
 
@@ -80,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 const uint16_t PROGMEM encoder_map[][1][2] = {
     [0] =  { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
     [1] =  { ENCODER_CCW_CW(KC_LEFT, KC_RIGHT)  },
-    [2] =  { ENCODER_CCW_CW(KC_HOME, KC_END)  },
+    [2] =  { ENCODER_CCW_CW(KC_UP, KC_DOWN)  },
     [3] =  { ENCODER_CCW_CW(KC_BRID, KC_BRIU) }
 };
 #endif
@@ -113,6 +115,24 @@ const uint16_t PROGMEM encoder_map[][1][2] = {
  *
  */
 int cur_dance (qk_tap_dance_state_t *state) {
+  if (!state->pressed) {
+    return 8;
+  }
+  switch (state->count) {
+    case 1:
+        return SINGLE_HOLD;
+    break;
+    case 2:
+        return DOUBLE_HOLD;
+    break;
+    case 3:
+        return TRIPLE_HOLD;
+    break;
+    case 4:
+        return QUAD_HOLD;
+    break;
+  }
+  /*
   if (state->count == 1) {
     if (state->interrupted || !state->pressed)  return 8;
     //key has not been interrupted, but they key is still held. Means you want to send a 'HOLD'.
@@ -133,6 +153,8 @@ int cur_dance (qk_tap_dance_state_t *state) {
     else return QUAD_HOLD;
   }
   else return 8; //magic number. At some point this method will expand to work for more presses
+  */
+ return 8;
 }
 
 //instanalize an instance of 'tap' for the 'x' tap dance.
