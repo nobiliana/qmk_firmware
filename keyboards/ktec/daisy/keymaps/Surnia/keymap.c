f8cd5802c443c0b76fae33dd6226dfc68672eee9
nobiliana <88566353+nobiliana@users.noreply.github.com>
2024-04-02 08:50:51 -0400

    Update keymap.c

--- keyboards/ktec/daisy/keymaps/Surnia/keymap.c
+++ keyboards/ktec/daisy/keymaps/Surnia/keymap.c
@@ -17,7 +17,6 @@
 #include QMK_KEYBOARD_H
 
 // Defines names for use in layer keycodes and the keymap
-// Defines names for use in layer keycodes and the keymap
 enum layer_names {
     _BASE,
     _FN1,
@@ -35,8 +34,8 @@ typedef enum {
   R1_HOLD,
   R2_HOLD,
   R3_HOLD,
-  SINGLE_TAP
-//  DOUBLE_TAP
+  L1_TAP
+//  L2_TAP
 } tappy_dance;
 
 typedef struct {
@@ -62,85 +61,41 @@ void LR_reset (tap_dance_state_t *state, void *user_data);
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* Base Layer
-     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
-     * │Esc│ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │ \ │
-     * ├───┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴───┤
-     * │Tab │ A │ S │ D │ F │ G │ H │ J │ K │ L │ Enter│
-     * ├────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬────┤
-     * │Shift │ Z │ X │ C │ V │ B │ N │ M │ , │ . │  / │
-     * ├────┬─┴─┬─┴──┬┴───┴───┼───┴───┴──┬┴───┼───┼────┤
-     * │Ctrl│GUI│Alt │        │ Backspace│Lowr│Rse│ Alt│
-     * └────┴───┴────┴────────┴──────────┴────┴───┴────┘
-     */
     [_BASE] = LAYOUT(
-        QK_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-        KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,             KC_ENT,
-        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-        KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,  KC_BSPC,                   LOWER,   RAISE,   KC_RALT
+        KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_BSPC,
+        KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,              KC_ENT,
+        KC_LSFT,           KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_RSFT, 
+        TD(LAYR), KC_LGUI, KC_LALT,          TD(LAYL),         KC_SPC,                    KC_RALT, KC_RCTL,  TD(LAYR)
     ),
 
-    /* Function Layer 1 (Lower)
-     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
-     * │ ` │ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 0 │ - │
-     * └───┴┬──┴┬──┴┬──┴───┴───┴┬──┴┬──┴┬──┴┬──┴┬──┴───┘
-     * │    │ = │ ▴ │   │   │   │ [ │ ] │ ; │ ' │      │
-     * ├────└─┬─┴─┬─┴─┬───┐─┴─┬─└───┴───┴───┴───┘─┬────┤
-     * │      │ ◂ │ ▾ │ ▸ │   │   │   │   │   │   │    │
-     * ├────┬─└───┴───┴───┘───┌──────────┐┴───┼───┼────┤
-     * │    │   │    │        │   Delete │    │   │    │
-     * └────┴───┴────┴────────└──────────┘────┴───┴────┘
-     */
     [_FN1] = LAYOUT(
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-        _______, KC_EQL,  KC_UP,   _______, _______, _______, KC_LBRC, KC_RBRC, KC_SCLN, KC_QUOT,          _______,
-        _______,          KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______,                   _______, KC_DEL,                    _______, _______, _______
+        KC_TRNS,  KC_ESC,  KC_PGUP, KC_GRV,  KC_ENT,  KC_P7,   KC_P8,   KC_P9,   KC_P0,   KC_PENT, C(KC_C),  KC_DEL,
+        KC_TRNS,  KC_HOME, KC_PGDN, KC_END,  C(KC_F), KC_P4,   KC_P5,   KC_P6,   KC_PPLS, KC_PAST,           C(KC_V),
+        KC_TRNS,           KC_SLSH, KC_MINS, KC_EQL,  KC_TAB,  KC_P1,   KC_P2,   KC_P3,   KC_PMNS, KC_PSLS,  _______,
+        KC_TRNS,  _______, _______,          KC_TRNS,          KC_PDOT,                   _______, _______,  KC_TRNS
     ),
 
-    /* Function Layer 2 (Raise)
-     * ┌───┬───┬───┬───┬───┬───┬───┐───┌───┬───┐───┬───┐
-     * │Rst│F1 │F2 │F3 │F4 │F5 │F6 │   │Hom│PgU│   │   │
-     * └───┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┐──└┬──┴┬──┴┐──┴───┤
-     * │    │F7 │F8 │F9 │F10│F11│F12│   │ ; │ ' │      │
-     * ├────└─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴───┴───┴───┴─┐────┤
-     * │      │RGB│Mod│HuD│HuI│VaD│VaI│SaD│SaI│BLS│    │
-     * ├────┬─└───┴───┴───┴───┴───┴───┴───┴───┴───┘────┤
-     * │    │   │    │        │          │    │   │    │
-     * └────┴───┴────┴────────┴──────────┴────┴───┴────┘
-     */
     [_FN2] = LAYOUT(
-        QK_BOOT,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, KC_HOME, KC_PGUP, _______, _______,
-        _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_END,  KC_PGDN,          _______,
-        _______,          RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI, RGB_VAD, RGB_VAI, RGB_SAD, RGB_SAI, BL_STEP, _______,
-        _______, _______, _______,                   _______, _______,                   _______, _______, _______
+        KC_TRNS,  KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC,  _______,
+        KC_TRNS,  KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_DLR,  KC_PERC, KC_CIRC, KC_SCLN, KC_QUOT,           KC_BSLS,
+        KC_TRNS,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_EXLM, KC_AT,   KC_HASH, _______, _______,  _______,
+        KC_TRNS,  _______, _______,          KC_TRNS,          _______,                   _______, _______,  KC_TRNS
     ),
 
-    /* Empty 4th layer for VIA
-     * ┌───┬───┬───┬───┬───┬───┬───┐───┌───┬───┐───┬───┐
-     * |   |   |   |   |   |   |   |   |   |   |   |   |
-     * └───┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┐──└┬──┴┬──┴┐──┴───┤
-     * │    │   │   │   │   │   │   │   │   │   │      │
-     * ├────└─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴───┴───┴───┴─┐────┤
-     * │      │   │   │   │   │   │   │   │   │   │    │
-     * ├────┬─└───┴───┴───┴───┴───┴───┴───┴───┴───┘────┤
-     * │    │   │    │        │          │    │   │    │
-     * └────┴───┴────┴────────┴──────────┴────┴───┴────┘
-     */
     [_FN3] = LAYOUT(
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        _______,  RGB_TOG, RGB_HUI, BL_TOGG, BL_UP,   _______, KC_PGUP, KC_HOME, KC_UP,   KC_END,  _______,  KC_INS,
+        _______,  RGB_MOD, RGB_SAI, BL_BRTG, BL_DOWN, _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT,           KC_PSCR,
+        _______,           RGB_SPI, RGB_VAI, _______, _______, _______, _______, KC_NUM,  KC_SCRL, KC_CAPS,  _______,
+        KC_TRNS,  _______, _______,          KC_TRNS,          _______,                   _______, _______,  KC_TRNS
     )
 };
 
 tappy_dance LAYL_dance (tap_dance_state_t *state) {
     switch (state->count) {
     case 1:
-      if (!state->pressed)  return SINGLE_TAP;
+      if (!state->pressed)  return L1_TAP;
       //only if key is released and NOT interrupted, but they key is still held. Means you want to send a 'HOLD'. Interrupt should lead to HOLD.
-      else return SINGLE_HOLD;
+      else return L1_HOLD;
     case 2:
         return L2_HOLD;
     case 3:
@@ -171,20 +126,20 @@ static tap ltap_state = {
 void LL_finished (tap_dance_state_t *state, void *user_data) {
   ltap_state.state = LAYL_dance(state);
   switch (ltap_state.state) {
-    case SINGLE_TAP: register_code(KC_SPC); break;
-//    case DOUBLE_TAP: register_code(KC_G); break;
-    case L1_HOLD: layer_on(_FN2); break;
-    case L2_HOLD: layer_on(_FN1); break;
+    case L1_TAP: register_code(KC_SPC); break;
+//    case L2_TAP: register_code(KC_G); break;
+    case L1_HOLD: layer_on(_FN1); break;
+    case L2_HOLD: layer_on(_FN2); break;
     case L3_HOLD: layer_on(_FN3); break;
     default: break;
   }
 }
 void LL_reset (tap_dance_state_t *state, void *user_data) {
   switch (ltap_state.state) {
-//    case SINGLE_TAP: unregister_code(KC_SPC); break;
-//    case DOUBLE_TAP: unregister_code(KC_G); break;
-    case L1_HOLD: layer_off(_FN2); break;
-    case L2_HOLD: layer_off(_FN1); break;
+    case L1_TAP: unregister_code(KC_SPC); break;
+//    case L2_TAP: unregister_code(KC_G); break;
+    case L1_HOLD: layer_off(_FN1); break;
+    case L2_HOLD: layer_off(_FN2); break;
     case L3_HOLD: layer_off(_FN3); break;
     default: break;
   }
@@ -198,21 +153,21 @@ static tap rtap_state = {
 void LR_finished (tap_dance_state_t *state, void *user_data) {
   rtap_state.state = LAYR_dance(state);  
   switch (rtap_state.state) {
-//    case SINGLE_TAP: register_code(KC_SPC); break;
-//    case DOUBLE_TAP: register_code(KC_G); break;
-    case R1_HOLD: layer_on(_FN1); break;
+//    case L1_TAP: register_code(KC_SPC); break;
+//    case L2_TAP: register_code(KC_G); break;
+    case R1_HOLD: layer_on(_FN3); break;
     case R2_HOLD: layer_on(_FN2); break;
-    case R3_HOLD: layer_on(_FN3); break;
+    case R3_HOLD: layer_on(_FN1); break;
     default: break;
   }
 }
 void LR_reset (tap_dance_state_t *state, void *user_data) {
   switch (rtap_state.state) {
-//    case SINGLE_TAP: unregister_code(KC_SPC); break;
-//    case DOUBLE_TAP: unregister_code(KC_G); break;
-    case R1_HOLD: layer_off(_FN1); break;
+//    case L1_TAP: unregister_code(KC_SPC); break;
+//    case L2_TAP: unregister_code(KC_G); break;
+    case R1_HOLD: layer_off(_FN3); break;
     case R2_HOLD: layer_off(_FN2); break;
-    case R3_HOLD: layer_off(_FN3); break;
+    case R3_HOLD: layer_off(_FN1); break;
     default: break;
   }
   rtap_state.state = TD_NONE;
